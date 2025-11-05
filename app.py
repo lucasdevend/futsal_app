846e9bda23bb610eefea78e8a7163ddd026bd949
lucasdevend <lucasdevend@gmail.com>
2025-11-05 13:23:12 -0300

    Atualiza app.py e adiciona requirements.txt

--- app.py
+++ app.py
@@ -417,5 +417,6 @@ threading.Thread(target=iniciar_agendador, daemon=True).start()
 # RODA O APP
 # ======================================
 if __name__ == "__main__":
-    print(app.url_map)
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
